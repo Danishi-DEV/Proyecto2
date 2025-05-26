dfdd8405050c1a79d3e6fd461f492d7d26f59712
Danishi-DEV <80767224+Danishi-DEV@users.noreply.github.com>
2025-05-25 22:11:33 -0500

    Update settings.py

--- Back/settings.py
+++ Back/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f#=c5z8!2h@00^^*jp8zvfo3$b&8a87rqup*5xp*sgdw+*=y72
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
